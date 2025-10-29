545dc3ce86a27b69e190764457af1fc4850dc0dd
jamesbowling86-gif <jamesbowling86@gmail.com>
2025-10-28 22:10:20 -0400

    Add files via upload

---
+++ PURPEFROGGERGIT/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FroggerGame from './FroggerGame'; // Make sure the path is correct
+
+function App() {
+  return (
+    <div className="App">
+      <FroggerGame />
+    </div>
+  );
+}
+
+export default App;
